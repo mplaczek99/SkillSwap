40e1cf5c2edf4fe284993fc2c056b42d814e740f
mplaczek99 <mplaczek99@gmail.com>
2025-02-05 01:47:18 -0600

    Added user.go

---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+// User represents a registered user in the SkillSwap platform.
+type User struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `gorm:"unique" json:"email"`
+	Password  string    `json:"-"` // Omit password from JSON responses
+	Bio       string    `json:"bio"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
